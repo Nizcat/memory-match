d20d2d8170031c9f5553c68f8d59ee03cc4635df
Nizcat <nizalli@gmail.com>
2022-11-10 06:31:33 -0600

    Bring data from Zelda´s Api and show images

--- src/components/App.js
+++ src/components/App.js
@@ -13,14 +13,64 @@
 //   .then(console.log)
 //   .catch(console.error);
 //
-
+import bringZelda from "../data/zeldaData/zelda.js";
 const App = () => {
+  const all = document.createElement('div');
+  const menu = document.createElement('div');
   const el = document.createElement('div');
 
-  el.className = 'App';
-  el.textContent = 'Hola mundo!';
+  menu.className = 'App';
+  menu.textContent = 'Zelda Elements';
 
-  return el;
+  function showZeldaMaterial() {
+    bringZelda('https://botw-compendium.herokuapp.com/api/v2/all')
+      .then((data) => Object.entries(data).forEach(([key, value]) => {
+        console.log(value, "allData");
+        const foodCreatures = value.monsters;
+        console.log(foodCreatures);
+        const allImages = document.createElement('div');
+        allImages.classList.add('allImagesCss');
+        foodCreatures.forEach((element) => {
+          const eachCard = document.createElement('div');
+          eachCard.classList.add('eachCard');
+          const zeldaImage = document.createElement('img');
+          zeldaImage.classList.add('zeldaImage');
+          zeldaImage.src = element.image;
+          const cardName = document.createElement('p');
+          cardName.classList.add('cardNameEach');
+          cardName.textContent = element.name;
+          eachCard.append(zeldaImage, cardName);
+          allImages.append(eachCard);
+        })
+        el.append(allImages)
+        all.append(menu, el);
+      }))
+  }
+  /*function showZeldaStaff() {
+    const allImages = document.createElement('div');
+
+    
+    bringZelda('https://zelda.fanapis.com/api/characters')
+      .then((element) => element.data.forEach(character => {
+        console.log(character);
+        const eachImage = document.createElement('img');
+        eachImage.classList.add('eachImageCss');
+        eachImage.src = character.appearances;
+        allImages.append(eachImage);
+      }));
+    el.append(allImages);
+
+
+
+  }*/
+
+
+
+
+
+  showZeldaMaterial();
+  return all;
+
 };
 
 export default App;
